748e0cae6520c2cd432118b0272c540e5a2a66bf
ayuindraswarii <71332092+ayuindraswarii@users.noreply.github.com>
2020-10-12 19:16:14 +0700

    Add files via upload

---
+++ penilaian.php
@@ -0,0 +1,13 @@
+<?php 
+
+$nilai = 90;//value var
+
+if ($nilai > 85) {
+	echo "Selamat... proses belajarmu sangat baik. Tingkatkan terus kemampuan codingnya";//menampilkan
+} elseif ($nilai > 70) {
+	echo "Selamat... kamu sudah mulai memahami pemrograman PHP. Tingkatkan terus skill codingmu";
+} elseif ($nilai >= 60) {
+	echo "Uppsssss... skill codingmu perlu diasah ini gaes, tetap semangat  belajar dan eksplore kemampuan ya";
+}
+
+ ?>
