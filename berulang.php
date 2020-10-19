5b12b75162d83d9679e9bb63c40120ba97870322
ayuindraswarii <71332092+ayuindraswarii@users.noreply.github.com>
2020-10-19 07:28:51 +0700

    Add files via upload

---
+++ berulang.php
@@ -0,0 +1,21 @@
+<?php 
+
+function halo_perkenalan()
+{
+	echo "Nama saya Ayu Indraswari";
+	echo "<br/>";
+	echo "Saya adalah mahasiswa Prodi Sistem Informasi";
+	echo "<br/>";
+	echo "Saya sangat tertarik mempelajari Teknik Informatika";
+}
+
+
+halo_perkenalan();
+echo "<br/><br/>";
+
+halo_perkenalan();
+
+ ?>
+
+
+
